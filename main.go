84dc7ddd1b922b5f7a3939fb8262c62da0fee8c7
Hector Chu <hectorchu@gmail.com>
2020-12-22 16:40:08 +0000

    Added reading

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"crypto/rand"
+	"errors"
+	"flag"
 	"fmt"
 	"io"
 	"math/big"
@@ -9,46 +11,86 @@ import (
 	"time"
 
 	"github.com/cheggaaa/pb/v3"
+	"github.com/hectorchu/gonano/rpc"
 	"github.com/hectorchu/gonano/util"
 	"github.com/hectorchu/gonano/wallet"
 )
 
-func fatal(err error) {
-	fmt.Println(err)
-	os.Exit(1)
+func main() {
+	var (
+		readAddress = flag.String("address", "", "read file from NANO address")
+		writeFile   = flag.String("file", "", "write file to a NANO address")
+		rpcURL      = flag.String("rpc", "https://mynano.ninja/api/node", "RPC URL to use")
+		err         = errors.New("Specify either -address or -file")
+	)
+	flag.Parse()
+	switch {
+	case *readAddress != "" && *writeFile != "":
+	case *readAddress != "":
+		err = read(*readAddress, *rpcURL)
+	case *writeFile != "":
+		err = write(*writeFile, *rpcURL)
+	default:
+	}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: nano-storage <file to store>")
+func read(address, rpcURL string) (err error) {
+	client := rpc.Client{URL: rpcURL}
+	info, err := client.AccountInfo(address)
+	if err != nil {
 		return
 	}
-	f, err := os.Open(os.Args[1])
+	hashes, err := client.Chain(info.Frontier, -1)
 	if err != nil {
-		fatal(err)
+		return
+	}
+	for i := len(hashes) - 2; i >= 0; i-- {
+		info, err := client.BlockInfo(hashes[i])
+		if err != nil {
+			return err
+		}
+		data, err := util.AddressToPubkey(info.Contents.Representative)
+		if err != nil {
+			return err
+		}
+		fmt.Print(string(data))
+	}
+	return
+}
+
+func write(name, rpcURL string) (err error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return
 	}
 	defer f.Close()
 	fi, err := f.Stat()
 	if err != nil {
-		fatal(err)
+		return
 	}
 	buf := make([]byte, 32)
-	if _, err := rand.Read(buf); err != nil {
-		fatal(err)
+	_, err = rand.Read(buf)
+	if err != nil {
+		return
 	}
 	w, err := wallet.NewWallet(buf)
 	if err != nil {
-		fatal(err)
+		return
 	}
+	w.RPC.URL = rpcURL
 	a, err := w.NewAccount(nil)
 	if err != nil {
-		fatal(err)
+		return
 	}
 	fmt.Println("Deposit some NANO to", a.Address())
 	for {
 		balance, pending, err := a.Balance()
 		if err != nil {
-			fatal(err)
+			return err
 		}
 		var zero big.Int
 		if balance.Cmp(&zero) > 0 || pending.Cmp(&zero) > 0 {
@@ -58,7 +100,7 @@ func main() {
 	}
 	err = a.ReceivePendings()
 	if err != nil {
-		fatal(err)
+		return
 	}
 	bar := pb.StartNew(int(fi.Size()))
 	for {
@@ -68,18 +110,19 @@ func main() {
 			break
 		}
 		if err != nil {
-			fatal(err)
+			return err
 		}
 		address, err := util.PubkeyToAddress(buf)
 		if err != nil {
-			fatal(err)
+			return err
 		}
 		_, err = a.ChangeRep(address)
 		if err != nil {
-			fatal(err)
+			return err
 		}
 		bar.Add(n)
 	}
 	bar.Finish()
 	fmt.Println("Stored", f.Name(), "to", a.Address())
+	return
 }
